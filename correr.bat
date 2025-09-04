f2628f30f15f96e431b6b17287fc18e3ab827a06
Fernando Lopez <frlopez88@icloud.com>
2025-09-04 15:01:03 -0600

    correcion del bat

--- correr.bat
+++ correr.bat
@@ -1,5 +1,13 @@
 @echo off
 cd /d "C:\Users\Administrator\Documents\GitHub\vozy-downloads"
-node index.js 
+for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (
+    set dia=%%a
+    set mes=%%b
+    set anio=%%c
+)
+
+set hoy=%anio%-%mes%-%dia%
+
+node index.js %hoy%
 
 exit /b 0
